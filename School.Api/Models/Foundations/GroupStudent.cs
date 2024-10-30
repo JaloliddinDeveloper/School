14e4671f067bf81d9dc33d96f0f4c164f974a4c3
jaloliddin.mahkamov@mail.ru <jaloliddin.mahkamov@mail.ru>
2024-10-31 02:10:53 +0500

    SERVICES: RetrieveGroupWithStudents Impelemnt

---
+++ School.Api/Models/Foundations/GroupStudent.cs
@@ -0,0 +1,16 @@
+﻿//--------------------------------------------------
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort And Peace
+//--------------------------------------------------
+using School.Api.Models.Foundations.Groups;
+using School.Api.Models.Foundations.Students;
+using System.Collections.Generic;
+
+namespace School.Api.Models.Foundations
+{
+    public class GroupStudent
+    {
+        public Group Group { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
---
+++ School.Api/Services/Orcestrations/OrcestrationService.cs
@@ -0,0 +1,39 @@
+﻿// Services/Orcestrations/OrcestrationService.cs
+using School.Api.Models.Foundations;
+using School.Api.Services.Foundations.Groups;
+using School.Api.Services.Foundations.Students;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.Api.Services.Orcestrations
+{
+    public class OrcestrationService : IOrcestrationService
+    {
+        private readonly IGroupService groupService;
+        private readonly IStudentService studentService;
+
+        public OrcestrationService(
+            IGroupService groupService,
+            IStudentService studentService)
+        {
+            this.groupService = groupService;
+            this.studentService = studentService;
+        }
+
+        public async ValueTask<IQueryable<GroupStudent>> RetrieveAllGroupsWithStudentsAsync()
+        {
+            // Fetch all groups and students separately
+            var groups = (await this.groupService.RetrieveAllGroupsAsync()).ToList();
+            var students = (await this.studentService.RetrieveAllStudentsAsync()).ToList();
+
+            // Link students to their respective groups in memory
+            var groupStudents = groups.Select(group => new GroupStudent
+            {
+                Group = group,
+                Students = students.Where(student => student.GroupId == group.Id).ToList()
+            });
+
+            return groupStudents.AsQueryable();
+        }
+    }
+}
